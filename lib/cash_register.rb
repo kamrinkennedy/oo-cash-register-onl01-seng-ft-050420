7a08fc904a59850328a9c41b170be3851b8a7719
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-15 20:21:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     self.discount = discount
     self.items = []
     self.prices = []
-    self.total = self.prices.sum.to_f
+    self.total = self.prices.sum.to
   end
   
   def add_item(name, price, quantity = 1)
