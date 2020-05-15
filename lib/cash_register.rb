9d6080c1e543b93490affb80a7bb94e1ede31e92
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-15 20:10:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     self.total += price * quantity
     quantity.times do 
       self.items << name
-      self.prices << price
+      self.prices << price.to_f
     end
   end
   
