db157653394ce8cfb123ccb78990923a445cc6f7
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-15 20:56:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,7 +31,7 @@ class CashRegister
   end
   
   def void_last_transaction
-    if self.prices.length = 0 
+    if self.prices.length == 0 
       self.total = 0
     else
       self.total -= self.prices[-1]
