c0577413a7f9a9dbb6a95e522f0fe3f2d3c34cc3
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-15 20:19:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   attr_accessor :total, :discount, :items, :prices
   
   def initialize(discount = 0)
-    self.total = 0
+    self.total = prices.sum
     self.discount = discount
     self.items = []
     self.prices = []
