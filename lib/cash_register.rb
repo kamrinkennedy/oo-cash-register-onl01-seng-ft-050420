d00b4e1b37d36a7be55de889a935b5f2e6a80a80
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-15 19:37:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
   
   def apply_discount
-    self.total = self.total - self.total*(self.discount/10)
+    self.total = self.total - self.total*(self.discount)
   end
   
 end
